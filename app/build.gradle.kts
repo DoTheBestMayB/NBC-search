bdb00f889f0e93aac914edcbd2b5673e03b48096
DoDoBest <send1684@gmail.com>
2024-04-30 15:33:19 +0900

    build: coil 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
+    implementation("io.coil-kt:coil:2.6.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
