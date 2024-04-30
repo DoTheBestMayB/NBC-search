d5a36bd53eb2973216d48ef05605aeb1f850b282
DoDoBest <send1684@gmail.com>
2024-04-30 14:40:39 +0900

    feat: MainActivity UI 설정

--- app/src/main/java/com/dothebestmayb/nbc_search/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/nbc_search/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.dothebestmayb.nbc_search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dothebestmayb.nbc_search.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setStatusBar()
     }
+
+    private fun setStatusBar() = with(binding) {
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
 }
